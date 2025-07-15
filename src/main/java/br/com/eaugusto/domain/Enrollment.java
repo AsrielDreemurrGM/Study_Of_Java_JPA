71dc625bef581942b890b56e4d050106dcbf575f
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-07-15 00:15:57 -0300

    Created Enrollment Entity Class In Domain Package
    - Mapped Entity To "tb_enrollment" Table Using JPA Annotations;
    - Defined Fields: Id, Code, Enrollment Date, Amount And Status;
    - Applied Constraints Like Length, Nullability, And Uniqueness;
    - Used "@SequenceGenerator" For Id Generation With Custom Sequence;

---
+++ src/main/java/br/com/eaugusto/domain/Enrollment.java
@@ -0,0 +1,77 @@
+package br.com.eaugusto.domain;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
+ * @since July 15, 2025
+ */
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "enrollment_seq")
+	@SequenceGenerator(name = "enrollment_seq", sequenceName = "sq_enrollment", initialValue = 1, allocationSize = 1)
+	private Long id;
+
+	@Column(name = "code", length = 10, nullable = false, unique = true)
+	private String code;
+
+	@Column(name = "enrollment_date", nullable = false)
+	private Instant enrollmentDate;
+
+	@Column(name = "amount", nullable = false)
+	private Double amount;
+
+	@Column(name = "status", nullable = false)
+	private String status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public Instant getEnrollmentDate() {
+		return enrollmentDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setEnrollmentDate(Instant enrollmentDate) {
+		this.enrollmentDate = enrollmentDate;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
