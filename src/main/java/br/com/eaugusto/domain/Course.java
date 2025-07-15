d1e1d0dc7d0305f81fdb69d4221d6292dc55bebc
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-07-15 00:00:28 -0300

    Create Course Domain Class
    - Created The "br.com.eaugusto.domain" Package For The File;
    - Mapped Class To "tb_course" Table Using JPA Annotations;
    - Defined Fields: Id, Code, Name, And Description;
    - Applied Constraints Like Length, Nullability, And Uniqueness;
    - Used "@SequenceGenerator" For Id Generation With Custom Sequence;

---
+++ src/main/java/br/com/eaugusto/domain/Course.java
@@ -0,0 +1,64 @@
+package br.com.eaugusto.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
+ * @since July 14, 2025
+ */
+@Entity
+@Table(name = "tb_course")
+public class Course {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_seq")
+	@SequenceGenerator(name = "course_seq", sequenceName = "sq_course", initialValue = 1, allocationSize = 1)
+	private Long id;
+
+	@Column(name = "code", length = 10, nullable = false, unique = true)
+	private String code;
+
+	@Column(name = "name", length = 50, nullable = false)
+	private String name;
+
+	@Column(name = "description", length = 100, nullable = false)
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
